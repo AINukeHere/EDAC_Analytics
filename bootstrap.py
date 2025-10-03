44f6f7fdf17ddc1c75de014eed1e11b874481c7f
AINukeHere <iii4625@naver.com>
2025-10-03 20:38:36 +0900

    2025.09

--- bootstrap.py
+++ bootstrap.py
@@ -5,8 +5,8 @@ from analytics import *
 analyzer = Analyzer()
 
 year = 2025
-month = 8
-weekDimStart = (2025, 7, 28)
+month = 9
+weekDimStart = (2025, 9, 1)
 
 # if True:
 checkFile = f'{year}년 {month}월 검색 유입 통계.xlsx'
